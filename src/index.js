7026576e7795866b77cdc5f9c57b636d1d07c779
Amanatri0 <atriaman8897@gmail.com>
2024-03-02 12:58:49 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 // require ('dotenv').config({path: './env'})   this syntax works fine but using import and require makes it inconsistent.
 
-import dotenv from "dotenv"
-import connectDB from "./dataBase/index.js"
-
+import dotenv from "dotenv";
+import connectDB from "./dataBase/index.js";
 
 dotenv.config({
-    path: "./env"
-})
-
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+      app.on("error", (error) => {
+        console.log(`Application is not Working `, error);
+        throw error;
+      });
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!!", err);
+  });
+
 
 
 
@@ -39,7 +50,7 @@ connectDB()
 //     try {
 //        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 //         app.on("error", (error) => {
-//             console.log("Something went wrong with the application while connecting to Database", error);
+//             console.log("Something went wrong with the application    while connecting to Database", error);
 //             throw error
 //         })
 
